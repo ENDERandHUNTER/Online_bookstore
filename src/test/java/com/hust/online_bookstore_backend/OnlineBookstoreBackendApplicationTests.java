966cfc5550c369e615ea04883a891c33043c3e8d
HUSTERandENDER <15272984770@163.com>
2024-12-18 20:31:07 +0800

    first

---
+++ src/test/java/com/hust/online_bookstore_backend/OnlineBookstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.online_bookstore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
